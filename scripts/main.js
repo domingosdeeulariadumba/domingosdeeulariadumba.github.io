775793a4ce77dba26a4b598f9eb642f806240b8c
domingosdeeulariadumba <domingosdeeularia@domingosdeeulariadumba.dominosdeeulariadumba>
2025-09-07 13:31:42 +0100

    Added js cript file

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Footer
+  const footer = document.querySelector("footer");
+  if (footer) {
+    footer.innerHTML = `© ${new Date().getFullYear()} <strong>Domingos de Eulária Dumba</strong>.`;
+  }
+
+  // Social links (optional if you want them here)
+  const socialLinks = document.getElementById("social-links");
+  if (socialLinks) {
+    const height = 20;
+    socialLinks.innerHTML = `
+      <a href='mailto:domingosdeeulariadumba@gmail.com' target='_blank'>
+        <img src='./assets/icons/contacts/email.png' height='${height}' width='${height}' />
+      </a>
+      <a href='https://github.com/domingosdeeulariadumba' target='_blank'>
+        <img src='./assets/icons/contacts/github.png' height='${height}' width='${height}' />
+      </a>
+      <a href='https://linkedin.com/in/domingosdeeulariadumba' target='_blank'>
+        <img src='./assets/icons/contacts/linkedin.png' height='${height}' width='${height}' />
+      </a>
+      <a href='https://ko-fi.com/domingosdeeulariadumba' target='_blank'>
+        <img src='./assets/icons/contacts/ko-fi.png' height='${height}' width='${height}' />
+      </a>
+      <a href='https://linktree.com/domingosdeeulariadumba' target='_blank'>
+        <img src='./assets/icons/contacts/linktree.png' height='${height}' width='${height}' />
+      </a>
+    `;
+  }
+});
